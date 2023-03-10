42b4132a620a4f6a8ea9f0b9e4bf19ba488e230f
Daniel Hernandez <danielhernandezmontero8@gmail.com>
2023-03-10 19:59:10 +0100

    a

--- EJ15/main.js
+++ EJ15/main.js
@@ -1,6 +1,9 @@
 const caracteres = ['🍍', '🍊', '🍉', '🍐', '🍑', '🍒', '🍓', '🍅', '🍆', '🌽', '🥭'];
 const array = [];
 
+
+let parrafo = document.getElementById("p");
+parrafo.innerHTML = array.toString();
 function charRandom() {
     return caracteres[Math.round(Math.random() * caracteres.length)];
 }
@@ -8,13 +11,12 @@ function charRandom() {
 // Push Boton
 document.getElementById("push").onclick = () => {
     array.push(charRandom());
-    console.log(array);
-};
+    parrafo.innerHTML = array.toString();};
 
 // Unshift Boton
 document.getElementById("unshift").onclick = () => {
     array.unshift(charRandom());
-    console.log(array);
+    parrafo.innerHTML = array.toString();
 };
 
 
@@ -23,19 +25,17 @@ document.getElementById("insert").onclick = () => {
     var valor = parseInt(document.getElementById("miInput").value);
     // var posicion = valor; // La posición se ajusta para comenzar en cero
     array.splice(valor, 0, (charRandom()));
-    console.log(array);
+    parrafo.innerHTML = array.toString();
 }
 // pop Boton
 document.getElementById("pop").onclick = () => {
     array.pop(charRandom());
-    console.log(array);
-};
+    parrafo.innerHTML = array.toString();};
 
 // shift Boton
 document.getElementById("shift").onclick = () => {
     array.shift(charRandom());
-    console.log(array);
-};
+    parrafo.innerHTML = array.toString();};
 
 
 // remove at Boton
@@ -43,5 +43,5 @@ document.getElementById("remove").onclick = () => {
     var valor = parseInt(document.getElementById("miInput2").value);
     // var posicion = valor; // La posición se ajusta para comenzar en cero
     array.splice(valor, 1);
-    console.log(array);
+    parrafo.innerHTML = array.toString();
 }
